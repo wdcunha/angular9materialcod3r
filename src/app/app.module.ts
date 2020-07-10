f687b2a661d3d80e949beef128d873c60d15f405
Wellington Cunha <wdcunha@gmail.com>
2020-07-10 18:10:45 +0100

    add product update

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -32,6 +32,7 @@ import {MatSortModule} from '@angular/material/sort';
 
 import localePt from '@angular/common/locales/pt';
 import {registerLocaleData} from '@angular/common';
+import { ProductUpdateComponent } from './components/product/product-update/product-update.component';
 
 registerLocaleData(localePt);
 
@@ -48,6 +49,7 @@ registerLocaleData(localePt);
     ProductCreateComponent,
     ProductReadComponent,
     ProductRead2Component,
+    ProductUpdateComponent,
   ],
   imports: [
     BrowserModule,
